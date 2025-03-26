3ab6cf602e2cf405fd2c620cbad7b593be0b0b5e
Jianting(Selina) <yumismilecode@gmail.com>
2025-03-26 11:59:57 -0400

    add env

--- run.py
+++ run.py
@@ -1,13 +1,23 @@
-#from app.routes.patient_routes import patient_bp  # 导入patient_bp
-
 from app import create_app
+import os
+from dotenv import load_dotenv
+
+# Load environment variables first
+load_dotenv()
+
+#print(os.environ)  
+
+# Read and print environment variable
+db_password = os.getenv('DB_PASSWORD')
+print(f"DB_PASSWORD: {db_password}")  # Ensure it's printed
+
 
 app = create_app()
-#app.register_blueprint(patient_bp, url_prefix='/patients')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
+
     print("Registered routes:")
     for rule in app.url_map.iter_rules():
         print(f"{rule} - {rule.endpoint}")
-# pip install flask-sqlalchemy
+    
